10196c7cf09d933d763ef0f9d5e42fa75eecadba
annalombari <89918689+annalombari@users.noreply.github.com>
2021-10-08 16:51:14 +0200

    Update Script.js

--- module4-solution/Script.js
+++ module4-solution/Script.js
@@ -1,1 +1,20 @@
 
+(function () {
+
+var names = ["Anna", "John", "Jen", "Jason", "Rob", "Frank", "Mark", "Paula", "Jack", "Simon"];
+
+for (var i = 0; i < names.length; i++) {
+  var firstLetter = names[i].charAt(0).toLowerCase();
+
+  if (firstLetter === 'j') {
+    byeSpeaker(names[i]);
+  }
+  else {
+    helloSpeaker(names[i]);
+  }
+}
+
+})();
+
+
+
